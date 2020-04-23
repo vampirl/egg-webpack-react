e12e37f1e1b6c2473e92b76fc6efb546f394ea4f
erchuang.li <erchuang.li@luckincoffee.com>
2020-04-23 22:29:32 +0800

    autoprefixer

--- app/controller/register.js
+++ app/controller/register.js
@@ -8,6 +8,13 @@ class RegisterController extends Controller {
     const result = await this.app.mysql.insert('user', { username: '江边', age: 17, sex: '男' });
     ctx.body = result;
   }
+  async getUsername() {
+    const { ctx } = this;
+    const query = ctx.query
+    const result = await this.app.mysql.query(`select * from user where id = ${query.id}`)
+    console.log(result)
+    ctx.body = result;
+  }
 }
 
 module.exports = RegisterController;
